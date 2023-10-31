746d7dc83528fc6bc4693cfa38dbdfd495a94a5e
gabe-ki70 <gabriel_koerner-imas26@milton.edu>
2023-10-31 13:07:23 -0400

    Made a new method that describes the pony

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello Object Factory!");
-        Ponies p = new Ponies();
+        Ponies p;
+        p = new Ponies();
         System.out.println(p.isUnicorn);
         p.isUnicorn = true;
         System.out.println(p.isUnicorn);
+        System.out.println(p.name);
+
+        p.describePony();
+
+        Ponies a;
+        a = new Ponies();
+        a.name = "Teddy";
+        System.out.println("pony a's name is " + a.name);
     }
 }
--- src/Ponies.java
+++ src/Ponies.java
@@ -3,12 +3,13 @@ public class Ponies {
     // numbers -> int, double
     //words -> Strings
 
-    boolean isUnicorn = false;
-    boolean isPegasus = false;
+    public boolean isUnicorn = false;
+    public boolean isPegasus = false;
+    public double size = 20.56;
+    public String name = "bobby";
+    public String color = "Pink";
 
-    double size = 20.56;
-    String name = "bobby";
-    String color = "Pink";
-
-
+    public void describePony(){
+        System.out.println("this is pony " + name + " they are the color " + color + " with a size of " + size + ". It is " + isUnicorn + " that the pony is a Unicorn. It is " + isPegasus + " that the pony is a pegasus.");
+    }
 }
