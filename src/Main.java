07bb11d2f05d0d700ec049a75eca675013c01a07
gabe-ki70 <gabriel_koerner-imas26@milton.edu>
2023-11-02 09:02:40 -0400

    Made parameters for our ponies

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,20 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello Object Factory!");
         Ponies p;
-        p = new Ponies();
-        System.out.println(p.isUnicorn);
-        p.isUnicorn = true;
+        p = new Ponies("Bobby", "Pink", 20.56, true, false);
         System.out.println(p.isUnicorn);
         System.out.println(p.name);
 
         p.describePony();
 
         Ponies a;
-        a = new Ponies();
-        a.name = "Teddy";
+        a = new Ponies("Teddy", "Red", 11.11, false, false);
         System.out.println("pony a's name is " + a.name);
+        a.describePony();
+
+        Ponies r;
+        r = new Ponies("Ronald", "Magenta", 16.42, false, true);
+        System.out.println("pony g's name is " + r.name);
+        r.describePony();
     }
 }
--- src/Ponies.java
+++ src/Ponies.java
@@ -3,11 +3,20 @@ public class Ponies {
     // numbers -> int, double
     //words -> Strings
 
-    public boolean isUnicorn = false;
-    public boolean isPegasus = false;
-    public double size = 20.56;
-    public String name = "bobby";
-    public String color = "Pink";
+    public boolean isUnicorn;
+    public boolean isPegasus;
+    public double size;
+    public String name;
+    public String color;
+
+    public Ponies(String pName, String pColor, double pSize, boolean pUnicorn, boolean pPegasus){
+        System.out.println("made a new pony!");
+        isUnicorn = pUnicorn;
+        isPegasus = pPegasus;
+        size = pSize;
+        name = pName;
+        color = pColor;
+    }
 
     public void describePony(){
         System.out.println("this is pony " + name + " they are the color " + color + " with a size of " + size + ". It is " + isUnicorn + " that the pony is a Unicorn. It is " + isPegasus + " that the pony is a pegasus.");
